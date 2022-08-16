1669b741388802a2df5875651765a3cef95a7ae7
ddthilindra <59472990+ddthilindra@users.noreply.github.com>
2022-08-16 19:54:42 +0530

    UI completed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,45 +1,111 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
+import React, { Component } from "react";
+import Paper from "@mui/material/Paper";
+import {
+  ViewState,
+  EditingState,
+  IntegratedEditing,
+} from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
   WeekView,
   Toolbar,
   DateNavigator,
   Appointments,
-  TodayButton,ViewSwitcher,MonthView,DayView,AppointmentForm,AppointmentTooltip
-} from '@devexpress/dx-react-scheduler-material-ui';
+  TodayButton,
+  ViewSwitcher,
+  MonthView,
+  DayView,
+  AppointmentForm,
+  AppointmentTooltip,
+  ConfirmationDialog,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
-import { appointments } from './appointments';
+import { appointments } from "./appointments";
 
-export default class Demo extends React.PureComponent {
+export default class Dashboard extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       data: appointments,
-      currentDate: '2018-06-27',
+      currentDate: "2018-06-27",
     };
-    this.currentDateChange = (currentDate) => { this.setState({ currentDate }); };
+    this.currentDateChange = (currentDate) => {
+      this.setState({ currentDate });
+    };
+    this.commitChanges = this.commitChanges.bind(this);
+  }
+ 
+
+  commitChanges({ added, changed, deleted }) {
+    this.setState((state) => {
+      let { data } = state;
+      if (added) {
+        const startingAddedId =
+          data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+        window.alert("added");
+      }
+      if (changed) {
+        data = data.map((appointment) =>
+          changed[appointment.id]
+            ? { ...appointment, ...changed[appointment.id] }
+            : appointment
+        );
+        window.alert("changed");
+      }
+      if (deleted !== undefined) {
+        data = data.filter((appointment) => appointment.id !== deleted);
+        window.alert("deleted");
+      }
+      return { data };
+    });
   }
 
   render() {
     const { data, currentDate } = this.state;
+    const saveAppoinment = () => {
+      window.alert("handleCellClick");
+    };
 
     return (
+      // <Paper>
+      //   <Scheduler data={data} height={660}>
+      //     <ViewState
+      //       currentDate={currentDate}
+      //       // onCurrentDateChange={this.currentDateChange}
+      //     />
+      //     <WeekView startDayHour={10} endDayHour={19} />
+      //     <WeekView
+      //       name="work-week"
+      //       displayName="Work Week"
+      //       excludedDays={[0, 6]}
+      //       startDayHour={9}
+      //       endDayHour={19}
+      //     />
+      //     <MonthView />
+
+      //     {/* <IntegratedEditing /> */}
+      //     <DayView startDayHour={9} endDayHour={19} />
+      //     <Toolbar />
+      //     <DateNavigator />
+      //     <ViewSwitcher />
+      //     <TodayButton />
+      //     {/* <ConfirmationDialog /> */}
+      //     <Appointments />
+      //     <EditingState onCommitChanges={this.commitChanges} />
+      //     <AppointmentTooltip showCloseButton showOpenButton />
+      //     <AppointmentForm />
+      //   </Scheduler>
+      // </Paper>
+
       <Paper>
-        <Scheduler
-          data={data}
-          height={660}
-        >
-          <ViewState
-            currentDate={currentDate}
-            onCurrentDateChange={this.currentDateChange}
-          />
-          <WeekView
-            startDayHour={10}
-            endDayHour={19}
-          />
+        <Scheduler data={data} height={660}>
+          <ViewState currentDate={currentDate} onCurrentDateChange={this.currentDateChange}/>
+          <EditingState onCommitChanges={this.commitChanges} />
+          <IntegratedEditing />
+          
+          <WeekView startDayHour={10} endDayHour={19} />
           <WeekView
             name="work-week"
             displayName="Work Week"
@@ -48,21 +114,18 @@ export default class Demo extends React.PureComponent {
             endDayHour={19}
           />
           <MonthView />
-          <DayView />
+          <DayView startDayHour={9} endDayHour={19} />
           <Toolbar />
           <DateNavigator />
           <ViewSwitcher />
           <TodayButton />
+          <ConfirmationDialog />
           <Appointments />
-          <AppointmentTooltip
-            showCloseButton
-            showOpenButton
-          />
-          <AppointmentForm
-            readOnly
-          />
+          <AppointmentTooltip showOpenButton showDeleteButton />
+          <AppointmentForm />
         </Scheduler>
       </Paper>
     );
   }
 }
+
